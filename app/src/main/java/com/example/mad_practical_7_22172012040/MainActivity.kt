31e2aaf239e692e1f972b3233b2125531ca87315
student <student@gnu.ac.in>
2023-09-22 12:31:39 +0530

    22-09

--- app/src/main/java/com/example/mad_practical_7_22172012040/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_7_22172012040/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mad_practical_7_22172012040
 
+import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,8 +12,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val mediaController = MediaController(this)
-        val uri:Uri = Uri.parse("android.resource://" + packageName + "/" +R.raw.thestoryoflight)
-        val myvideoview =findViewById<VideoView>(R.id.videoView)
+        val videoView: VideoView=findViewById(R.id.videoView)
+        val button: FloatingActionButton= findViewById(R.id.floatingActionButton)
+        videoplayer()
+        button.setOnClickListener {
+            Intent(this@MainActivity,YoutubeActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
+    fun videoplayer(){
+        val myvideoView: VideoView=findViewById(R.id.videoView)
+        val mediaController=MediaController(this)
+        val uri: Uri =Uri.parse("android.resource://"+packageName+"/"+R.raw.thestoryoflight)
+        myvideoView.setMediaController(mediaController)
+        mediaController.setAnchorView(myvideoView)
+        myvideoView.setVideoURI(uri)
+        myvideoView.requestFocus()
+        myvideoView.start()
+    }
+
 }
--- app/src/main/java/com/example/mad_practical_7_22172012040/YoutubeActivity.kt
+++ app/src/main/java/com/example/mad_practical_7_22172012040/YoutubeActivity.kt
@@ -2,10 +2,17 @@ package com.example.mad_practical_7_22172012040
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
 
 class YoutubeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_youtube)
+        val YoutubeID = "fJn9B64Znrk"
+        fun YTPlayer()
+        {
+            val webview = findViewById<WebView>(R.id.webView)
+        }
     }
 }
